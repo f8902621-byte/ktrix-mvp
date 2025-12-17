feff8095e2dd3b1cfd444bba694f00e7fee23e60
f8902621-byte <f8902621@gmail.com>
2025-12-17 11:12:32 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
-    domains: [
-      'cdn.propertyguru.com.vn',
-      'cdn.propertyguru.com.sg',
-      'via.placeholder.com',
-      'images.unsplash.com'
-    ],
     unoptimized: true
   },
-  env: {
-    APIFY_API_TOKEN: process.env.APIFY_API_TOKEN,
-    APIFY_ACTOR_ID: process.env.APIFY_ACTOR_ID || 'k9WTLB7RrdLVj9Gyp',
-  }
+  output: 'standalone'
 }
 
 module.exports = nextConfig
