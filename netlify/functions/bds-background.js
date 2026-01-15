3f3d715815e146697f93fea79dde45ad7a94a693
f8902621-byte <f8902621@gmail.com>
2026-01-15 14:57:54 +0700

    Update bds-background.js

--- netlify/functions/bds-background.js
+++ netlify/functions/bds-background.js
@@ -329,7 +329,7 @@ async function scrapeBdsListPage(city, propertyType, priceMax) {
   
   console.log(`BDS Background: Scraping liste ${bdsUrl}`);
   
-  cconst scraperUrl = `https://api.scraperapi.com/?api_key=${SCRAPER_API_KEY}&url=${encodeURIComponent(bdsUrl)}&render=true`;
+  const scraperUrl = `https://api.scraperapi.com/?api_key=${SCRAPER_API_KEY}&url=${encodeURIComponent(bdsUrl)}&render=true`;
   const response = await fetch(scraperUrl);
   
   if (!response.ok) {
