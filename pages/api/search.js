f9c5979b2b4b66189605fed204b15874b3068e3a
f8902621-byte <f8902621@gmail.com>
2026-01-20 09:18:02 +0700

    Update search.js

--- pages/api/search.js
+++ pages/api/search.js
@@ -5,7 +5,7 @@
 
 // import { computeKOS } from '../../lib/Scoring.js';
 
-
+console.log("=== EXECUTING /pages/api/search.js ===");
 
 const APIFY_API_TOKEN = process.env.APIFY_API_TOKEN;
 const APIFY_ACTOR_ID = process.env.APIFY_ACTOR_ID;
