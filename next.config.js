71387c75292c8fed7849189ce342576c786cba89
f8902621-byte <f8902621@gmail.com>
2026-03-30 13:55:10 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,20 @@ const nextConfig = {
   swcMinify: true,
   images: {
     unoptimized: true
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+          { key: 'X-XSS-Protection', value: '1; mode=block' },
+          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
